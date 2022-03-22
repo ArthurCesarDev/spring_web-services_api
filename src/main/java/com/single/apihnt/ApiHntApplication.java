96f616cca950ae96db72b0eb5856cedbed7969e6
Arthur Cesar Araujo <arthurcesarx@outlook.com>
2022-03-22 19:11:53 -0300

    Project created

---
+++ src/main/java/com/single/apihnt/ApiHntApplication.java
@@ -0,0 +1,13 @@
+package com.single.apihnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/single/apihnt/ApiHntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.single.apihnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
