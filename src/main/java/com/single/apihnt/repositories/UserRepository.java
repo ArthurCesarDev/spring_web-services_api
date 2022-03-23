3f4116e91e3fa215f6a58fcf884a3802002ec94e
Arthur Cesar Araujo <arthurcesarx@outlook.com>
2022-03-22 21:11:58 -0300

    Inserir dados tabelas

---
+++ src/main/java/com/single/apihnt/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.single.apihnt.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.single.apihnt.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
